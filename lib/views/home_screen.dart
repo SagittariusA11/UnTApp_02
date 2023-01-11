34ba239ee6d7edd7efc1f7db35f8f3f6e680199e
yashsomalkar <yashsomalkar@gmail.com>
2023-01-11 09:26:34 +0530

    unt_02_UI

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,21 +1,30 @@
-import 'package:flutter/material.dart';
-
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Text("Home Screen"),
-        ),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:get/get_core/src/get_main.dart';
+// import 'package:unt_02/views/Anonynmous_main_screen.dart';
+//
+// import '../controller/data_controller.dart';
+//
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   State<HomeScreen> createState() => _HomeScreenState();
+// }
+//
+// class _HomeScreenState extends State<HomeScreen> {
+//   final DataController controller = Get.put(DataController());
+//   @override
+//   void initState() {
+//     WidgetsBinding.instance.addPostFrameCallback((_) async {
+//       await controller.getUserProfileData();
+//       setState(() {});
+//     });
+//     super.initState();
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return  screen2();
+//
+//   }
+// }
